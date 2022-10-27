1f42455f1d58b56270aa8aea4fb6b2178e503c80
Caio Vinícius <caio1525pereira@gmail.com>
2022-10-27 11:49:24 -0300

    Rotas Públicas - Aplicando navegação de autenticação

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,11 +1,21 @@
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import { useNavigation } from "@react-navigation/native";
 
-import LogoSvg from "@assets/logo.svg";
-import BackgroundImg from "@assets/background.png";
+import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
+
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
+import LogoSvg from "@assets/logo.svg";
+import BackgroundImg from "@assets/background.png";
+
 export function SignIn() {
+	const navigation = useNavigation<AuthNavigatorRoutesProps>();
+
+	function handleNewAccount() {
+		navigation.navigate("signUp");
+	}
+
 	return (
 		<ScrollView
 			contentContainerStyle={{
@@ -49,7 +59,11 @@ export function SignIn() {
 					<Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
 						Ainda não tem acesso?
 					</Text>
-					<Button title="Criar conta" variant="outline" />
+					<Button
+						title="Criar conta"
+						variant="outline"
+						onPress={handleNewAccount}
+					/>
 				</Center>
 			</VStack>
 		</ScrollView>
