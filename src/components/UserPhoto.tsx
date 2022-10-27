2caa2d60d27c4793e19bc6cb9302f9e1b8142722
Caio Vinícius <caio1525pereira@gmail.com>
2022-10-27 20:47:12 -0300

    Finalizando as Interfaces - Componente UserPhoto

---
+++ src/components/UserPhoto.tsx
@@ -0,0 +1,19 @@
+import { Image, IImageProps } from "native-base";
+
+interface UserPhotoProps extends IImageProps {
+	size: number;
+}
+
+export function UserPhoto({ size, ...props }: UserPhotoProps) {
+	return (
+		<Image
+			w={size}
+			h={size}
+			rounded="full"
+			borderWidth={2}
+			borderColor="gray.400"
+			alt="Imagem do usuário"
+			{...props}
+		/>
+	);
+}
