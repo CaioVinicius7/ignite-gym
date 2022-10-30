82539029a66a5a67e83733498cf106d9e1b33c69
Caio Vinícius <caio1525pereira@gmail.com>
2022-10-29 21:38:21 -0300

    Finalizando as Interfaces - Foto do usuário

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,11 +1,23 @@
-import { VStack } from "native-base";
+import { Center, ScrollView, VStack } from "native-base";
 
 import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
 
 export function Profile() {
 	return (
 		<VStack flex={1}>
 			<ScreenHeader title="Perfil" />
+
+			<ScrollView>
+				<Center mt={6} px={10}>
+					<UserPhoto
+						source={{
+							uri: "https://github.com/caiovinicius7.png"
+						}}
+						size={33}
+					/>
+				</Center>
+			</ScrollView>
 		</VStack>
 	);
 }
