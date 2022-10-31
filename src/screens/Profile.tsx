76fec03b72b8a6dda589a87264ffad603e6a70c7
Caio Vinícius <caio1525pereira@gmail.com>
2022-10-30 23:20:12 -0300

    Galeria de Fotos - Exibindo a foto selecionada

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -19,6 +19,9 @@ const PHOTO_SIZE = 33;
 
 export function Profile() {
 	const [photoIsLoading, setPhotoIsLoading] = useState(false);
+	const [userPhoto, setUserPhoto] = useState(
+		"https://github.com/caiovinicius7.png"
+	);
 
 	async function handleUserPhotoSelect() {
 		const photoSelected = await ImagePicker.launchImageLibraryAsync({
@@ -32,7 +35,7 @@ export function Profile() {
 			return;
 		}
 
-		console.log(photoSelected);
+		setUserPhoto(photoSelected.uri);
 	}
 
 	return (
@@ -56,7 +59,7 @@ export function Profile() {
 					) : (
 						<UserPhoto
 							source={{
-								uri: "https://github.com/caiovinicius7.png"
+								uri: userPhoto
 							}}
 							size={PHOTO_SIZE}
 						/>
