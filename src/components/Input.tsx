a3cfe5e61e9aa85c5bd915de8a7c0213eeb473a8
Caio Vinícius <caio1525pereira@gmail.com>
2022-10-31 17:09:30 -0300

    Formulários - Estilizando mensagens de validação

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,23 +1,40 @@
-import { Input as NativeBaseInput, IInputProps } from "native-base";
+import {
+	Input as NativeBaseInput,
+	IInputProps,
+	FormControl
+} from "native-base";
 
-export function Input({ ...props }: IInputProps) {
+interface InputProps extends IInputProps {
+	errorMessage?: string | null;
+}
+
+export function Input({
+	errorMessage = null,
+	isInvalid,
+	...props
+}: InputProps) {
+	const invalid = !!errorMessage || isInvalid;
+
 	return (
-		<NativeBaseInput
-			bg="gray.700"
-			h={14}
-			px={4}
-			borderWidth={0}
-			fontSize="md"
-			color="white"
-			fontFamily="body"
-			mb={4}
-			placeholderTextColor="gray.300"
-			_focus={{
-				bg: "gray.700",
-				borderWidth: 1,
-				borderColor: "green.500"
-			}}
-			{...props}
-		/>
+		<FormControl isInvalid={invalid} mb={4}>
+			<NativeBaseInput
+				bg="gray.700"
+				h={14}
+				px={4}
+				borderWidth={0}
+				fontSize="md"
+				color="white"
+				fontFamily="body"
+				placeholderTextColor="gray.300"
+				_focus={{
+					bg: "gray.700",
+					borderWidth: 1,
+					borderColor: "green.500"
+				}}
+				{...props}
+			/>
+
+			<FormControl.ErrorMessage>{errorMessage}</FormControl.ErrorMessage>
+		</FormControl>
 	);
 }
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -80,11 +80,14 @@ export function SignUp() {
 							required: "Informe o nome"
 						}}
 						render={({ field: { value, onChange } }) => (
-							<Input placeholder="Nome" onChangeText={onChange} value={value} />
+							<Input
+								placeholder="Nome"
+								onChangeText={onChange}
+								value={value}
+								errorMessage={errors.name?.message}
+							/>
 						)}
 					/>
-
-					<Text color="white">{errors.name?.message}</Text>
 
 					<Controller
 						control={control}
@@ -103,11 +106,10 @@ export function SignUp() {
 								autoCapitalize="none"
 								onChangeText={onChange}
 								value={value}
+								errorMessage={errors.email?.message}
 							/>
 						)}
 					/>
-
-					<Text color="white">{errors.email?.message}</Text>
 
 					<Controller
 						control={control}
