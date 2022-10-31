9d196c6cf400ce809bdc2a0783183ef739452ca8
Caio Vinícius <caio1525pereira@gmail.com>
2022-10-31 17:01:05 -0300

    Formulários - Obtendo a mensagem de erro

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -16,7 +16,11 @@ type FormDataProps = {
 };
 
 export function SignUp() {
-	const { control, handleSubmit } = useForm<FormDataProps>({
+	const {
+		control,
+		handleSubmit,
+		formState: { errors }
+	} = useForm<FormDataProps>({
 		defaultValues: {
 			name: "",
 			email: "",
@@ -79,6 +83,8 @@ export function SignUp() {
 							<Input placeholder="Nome" onChangeText={onChange} value={value} />
 						)}
 					/>
+
+					<Text color="white">{errors.name?.message}</Text>
 
 					<Controller
 						control={control}
