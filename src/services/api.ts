b241a72f43af5b05cc3c8826f9f0649234a13d58
Caio Vinícius <caio1525pereira@gmail.com>
2022-11-27 22:26:04 -0300

    HTTP Client - Entendendo Interceptors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,32 @@
 import axios from "axios";
 
-export const api = axios.create({
+const api = axios.create({
 	baseURL: "http://192.168.0.137:3333"
 });
+
+api.interceptors.request.use(
+	(config) => {
+		console.log("INTERCEPTOR REQUEST:", config);
+		return config;
+	},
+	(error) => {
+		console.log("INTERCEPTOR REQUEST ERROR:", error);
+
+		return Promise.reject(error);
+	}
+);
+
+api.interceptors.response.use(
+	(response) => {
+		console.log("INTERCEPTOR RESPONSE:", response);
+
+		return response;
+	},
+	(error) => {
+		console.log("INTERCEPTOR RESPONSE ERROR:", error);
+
+		return Promise.reject(error);
+	}
+);
+
+export { api };
