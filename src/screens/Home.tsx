1b530ccbf3da7c597c53d4f43e9ba2ea7edac40b
Caio Vinícius <caio1525pereira@gmail.com>
2022-11-29 11:38:49 -0300

    Consumindo o Back-end - Buscando Exercícios por grupo

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { HStack, VStack, FlatList, Heading, Text, useToast } from "native-base";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 
 import { AppNavigatorRoutesProps } from "@routes/app.routes";
 
@@ -14,12 +14,7 @@ import { AppError } from "@utils/AppError";
 
 export function Home() {
 	const [groups, setGroups] = useState<string[]>([]);
-	const [exercises, setExercises] = useState([
-		"Puxada frontal",
-		"Remada curvada",
-		"Remada unilateral",
-		"Levantamento terra"
-	]);
+	const [exercises, setExercises] = useState([]);
 	const [groupSelected, setGroupSelected] = useState("costas");
 
 	const toast = useToast();
@@ -50,9 +45,35 @@ export function Home() {
 		}
 	}
 
+	async function fetchExercisesByGroup() {
+		try {
+			const response = await api.get(`/exercises/bygroup/${groupSelected}`);
+
+			console.log(response.data);
+		} catch (error) {
+			const isAppError = error instanceof AppError;
+
+			const title = isAppError
+				? error.message
+				: "Não foi possível carregar os exercícios.";
+
+			toast.show({
+				title,
+				placement: "top",
+				bgColor: "red.500"
+			});
+		}
+	}
+
 	useEffect(() => {
 		fetchGroups();
 	}, []);
+
+	useFocusEffect(
+		useCallback(() => {
+			fetchExercisesByGroup();
+		}, [groupSelected])
+	);
 
 	return (
 		<VStack flex={1}>
