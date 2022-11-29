15e3dd580a71978db78317a8df6a9651bd70f77b
Caio Vinícius <caio1525pereira@gmail.com>
2022-11-29 11:07:48 -0300

    Autenticação JWT - Recuperando o token do storage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -9,7 +9,10 @@ import {
 	storageUserRemove,
 	storageUserSave
 } from "@storage/storageUser";
-import { storageAuthTokenSave } from "@storage/storageAuthToken";
+import {
+	storageAuthTokenGet,
+	storageAuthTokenSave
+} from "@storage/storageAuthToken";
 export interface AuthContextDataProps {
 	user: UserDTO;
 	isLoadingUserStorageData: boolean;
@@ -30,15 +33,17 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 	const [isLoadingUserStorageData, setIsLoadingUserStorageData] =
 		useState(true);
 
-	async function storageUserAndToken(userData: UserDTO, token: string) {
+	async function userAndTokenUpdate(userData: UserDTO, token: string) {
+		api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+		setUser(userData);
+	}
+
+	async function storageUserAndTokenSave(userData: UserDTO, token: string) {
 		try {
 			setIsLoadingUserStorageData(true);
 
-			api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-
 			await storageUserSave(userData);
 			await storageAuthTokenSave(token);
-			setUser(userData);
 		} catch (error) {
 			throw error;
 		} finally {
@@ -54,7 +59,8 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 			});
 
 			if (data.user && data.token) {
-				storageUserAndToken(data.user, data.token);
+				await storageUserAndTokenSave(data.user, data.token);
+				userAndTokenUpdate(data.user, data.token);
 			}
 		} catch (error) {
 			throw error;
@@ -77,10 +83,13 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
 	async function loadUserData() {
 		try {
-			const userLogged = await storageUserGet();
+			setIsLoadingUserStorageData(true);
 
-			if (userLogged) {
-				setUser(userLogged);
+			const userLogged = await storageUserGet();
+			const token = await storageAuthTokenGet();
+
+			if (userLogged && token) {
+				userAndTokenUpdate(userLogged, token);
 			}
 		} catch (error) {
 			throw error;
