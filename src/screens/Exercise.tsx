909ea00219d96cf99490a4d1aeff3eb3daec7965
Caio Vinícius <caio1525pereira@gmail.com>
2022-11-29 21:05:36 -0300

    Consumindo o Back-end - Registrando histórico

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -35,6 +35,7 @@ interface RouteParamsProps {
 
 export function Exercise() {
 	const [isLoading, setIsLoading] = useState(true);
+	const [sendRegister, setSendRegister] = useState(false);
 	const [exercise, setExercise] = useState<ExerciseDTO>({} as ExerciseDTO);
 
 	const navigation = useNavigation<AppNavigatorRoutesProps>();
@@ -70,6 +71,36 @@ export function Exercise() {
 			});
 		} finally {
 			setIsLoading(false);
+		}
+	}
+
+	async function handleExerciseHistoryRegister() {
+		try {
+			setSendRegister(true);
+
+			await api.post("/history", {
+				exercise_id: exerciseId
+			});
+
+			toast.show({
+				title: "Parabéns! Exercício registrado no seu histórico.",
+				placement: "top",
+				bgColor: "green.700"
+			});
+		} catch (error) {
+			const isAppError = error instanceof AppError;
+
+			const title = isAppError
+				? error.message
+				: "Não foi possível registrar o exercício.";
+
+			toast.show({
+				title,
+				placement: "top",
+				bgColor: "red.500"
+			});
+		} finally {
+			setSendRegister(false);
 		}
 	}
 
@@ -149,7 +180,11 @@ export function Exercise() {
 								</HStack>
 							</HStack>
 
-							<Button title="Marcar como realizado" />
+							<Button
+								title="Marcar como realizado"
+								isLoading={sendRegister}
+								onPress={handleExerciseHistoryRegister}
+							/>
 						</Box>
 					</VStack>
 				</ScrollView>
