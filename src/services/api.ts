30f81087a141b9b3ebc6716004f8a0413b6231a4
Caio Vinícius <caio1525pereira@gmail.com>
2022-12-02 12:36:38 -0300

    Refresh Token - Incluindo gerenciador de Token na API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,20 +1,32 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
 
 import { AppError } from "@utils/AppError";
 
+type signOut = () => void;
+
+interface APIInstanceProps extends AxiosInstance {
+	registerInterceptTokenManager: (signOut: signOut) => () => void;
+}
+
 const api = axios.create({
 	baseURL: process.env.BASE_URL as string
-});
+}) as APIInstanceProps;
 
-api.interceptors.response.use(
-	(response) => response,
-	(error) => {
-		if (error.response && error.response.data) {
-			return Promise.reject(new AppError(error.response.data.message));
+api.registerInterceptTokenManager = (signOut) => {
+	const interceptTokenManager = api.interceptors.response.use(
+		(response) => response,
+		(error) => {
+			if (error.response && error.response.data) {
+				return Promise.reject(new AppError(error.response.data.message));
+			}
+
+			return Promise.reject(new AppError(error));
 		}
+	);
 
-		return Promise.reject(new AppError(error));
-	}
-);
+	return () => {
+		api.interceptors.response.eject(interceptTokenManager);
+	};
+};
 
 export { api };
